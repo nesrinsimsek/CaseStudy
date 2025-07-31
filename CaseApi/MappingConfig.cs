3354184fb6f7c2f397075f14ed366098c146ee8e
Nesrin Şimşek <93262988+nesrinsimsek@users.noreply.github.com>
2025-07-31 17:57:29 +0300

    auto mapper package installed and mapping implemented for dtos

---
+++ CaseApi/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CaseApi.Dtos;
+using CaseDataAccess.Entities.Concrete;
+
+namespace CaseApi
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+
+            CreateMap<TodoItem, TodoItemCreateDto>().ReverseMap();
+            CreateMap<TodoItem, TodoItemDto>().ReverseMap();
+            CreateMap<TodoItem, TodoItemUpdateDto>().ReverseMap();
+
+            CreateMap<User, UserDto>().ReverseMap();
+
+        }
+    }
+}
--- CaseApi/Program.cs
+++ CaseApi/Program.cs
@@ -1,4 +1,5 @@
 
+using CaseApi;
 using CaseBusiness.DependencyInjection;
 using CaseDataAccess;
 using CaseDataAccess.DependencyInjection;
@@ -9,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options => options.
     UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
